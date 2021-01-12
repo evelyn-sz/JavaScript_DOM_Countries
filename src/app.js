b02ee0d6016c60edbcef86aea962bf4683bb7b81
evelyn-sz <evelynszarek@gmail.com>
2021-01-12 16:31:06 +0000

    Time's up

--- src/app.js
+++ src/app.js
@@ -4,10 +4,30 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue( {
         el: '#app',
         data: {
-
+            allCountries: null,
+            selectedCountry: null,
+            favouriteCountries: []
+        },
+        mounted: function () {
+            this.fetchCountries()
+        },
+        computed: {
+            totalPopulation: function(){
+                return this.allCountries.reduce(( total, country ) => total + country.population, 0)
+            }
         },
         methods: {
-            pass
+            fetchCountries: function() {
+                fetch("https://restcountries.eu/rest/v2/all")
+                .then( response => response.json() )
+                .then( data => this.allCountries = data)
+            },
+            selectCountry: function(countryIndex){
+                this.selectedCountry = this.allCountries[countryIndex]
+            },
+            addToFavourites: function (countryIndex) {
+                this.favouriteCountries.push(this.allCountries[countryIndex] )
+            }
         }
     })
 })
